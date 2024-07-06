81312956b42d7b4cf0c44123ff970f400937050b
Ray Burks <ray_burks@outlook.com>
2024-07-06 10:27:51 +0200

    add new projects

--- Aspire.ServiceBusMessaging/Aspire.ServiceBusMessaging.AppHost/Program.cs
+++ Aspire.ServiceBusMessaging/Aspire.ServiceBusMessaging.AppHost/Program.cs
@@ -1,11 +1,13 @@
+using Aspire.Hosting;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.Aspire_ServiceBusMessaging_WeatherForecastApi>("aspire.servicebusmessaging.weatherforecastapi");
+//builder.AddProject<Projects.Aspire_ServiceBusMessaging_WeatherForecastApi>("aspire.servicebusmessaging.weatherforecastapi");
 
-builder.AddProject<Projects.Aspire_ServiceBusMessaging_DemoSubscriptonService>("aspire.servicebusmessaging.demosubscriptonservice");
+//builder.AddProject<Projects.Aspire_ServiceBusMessaging_DemoSubscriptonService>("aspire.servicebusmessaging.demosubscriptonservice");
 
-builder.AddProject<Projects.Aspire_ServiceBusMessaging_ProxyApi>("aspire.servicebusmessaging.proxyapi");
+//builder.AddProject<Projects.Aspire_ServiceBusMessaging_ProxyApi>("aspire.servicebusmessaging.proxyapi");
 
-builder.AddProject<Projects.Aspire_ServiceBusMessaging_SampleSubscriptionService>("aspire.servicebusmessaging.samplesubscriptionservice");
+//builder.AddProject<Projects.Aspire_ServiceBusMessaging_SampleSubscriptionService>("aspire.servicebusmessaging.samplesubscriptionservice");
 
 builder.Build().Run();
